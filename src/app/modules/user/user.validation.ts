58a70319da86c3608778a9caf2a105269c21e91e
Your Name <khalidsaifullaha3@gmail.com>
2025-10-08 22:16:12 +0600

    Implementing Request Validation Middleware & Patient Creation with Zod Schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,5 +1,14 @@
-import express, { NextFunction, Request, Response } from "express";
+import z from "zod";
 
-const router = express.Router();
+const createPatientValidationSchema = z.object({
+  password: z.string(),
+  patient: z.object({
+    name: z.string().nonempty("Name is required"),
+    email: z.string().nonempty("Email is required"),
+    address: z.string().optional(),
+  }),
+});
 
-export const userRoutes = router;
+export const UserValidation = {
+  createPatientValidationSchema,
+};
